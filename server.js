218da3a821936a2d0dd74f488b3c197554e4ca53
Feyz Ibrahim <faizashrafkk@gmail.com>
2023-07-12 18:34:50 +0530

    initial works done

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
+// importing process
 require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+// Importing routes created by me
+const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// use the imported routes here
+app.use("/api/workouts", workoutRoutes);
+
+// Printing the API routes // Remove before publishing
 app.use((req, res, next) => {
   console.log(req.path, req.method);
+  next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to parallel backend" });
-});
-
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+// Connect to atlas
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listens on app
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & Listening on port " + process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
